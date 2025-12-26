168f4c94d0a8b1ff3e9d0c35184a8b3791c69f28
LileeeC <lileee0801@gmail.com>
2025-12-26 11:33:01 +0800

    init commit

---
+++ 3/3_1/My_Kernel.c
@@ -0,0 +1,59 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <asm/current.h>
+
+#define procfs_name "Mythread_info"
+#define BUFSIZE  1024
+char buf[BUFSIZE];
+
+static ssize_t Mywrite(struct file *fileptr, const char __user *ubuf, size_t buffer_len, loff_t *offset){
+    /* Do nothing */
+	return 0;
+}
+
+
+static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len, loff_t *offset){
+    /*Your code here*/
+    int len = 0;
+    struct task_struct *t;
+
+    if (*offset > 0) return 0; // EOF
+
+    len += scnprintf(buf + len, BUFSIZE - len,
+                     "Process ID: %d\n", current->pid);
+
+    for_each_thread(current, t) {
+        len += scnprintf(buf + len, BUFSIZE - len,
+                         "Thread ID: %d, Priority: %d, State: %ld\n",
+                         t->pid, t->prio, t->__state);
+        if (len >= BUFSIZE) break;
+    }
+
+    if (copy_to_user(ubuf, buf, len)) return -EFAULT;
+    *offset += len;
+    return len;
+    /****************/
+}
+
+static struct proc_ops Myops = {
+    .proc_read = Myread,
+    .proc_write = Mywrite,
+};
+
+static int My_Kernel_Init(void){
+    proc_create(procfs_name, 0644, NULL, &Myops);   
+    pr_info("My kernel says Hi");
+    return 0;
+}
+
+static void My_Kernel_Exit(void){
+    pr_info("My kernel says GOODBYE");
+}
+
+module_init(My_Kernel_Init);
+module_exit(My_Kernel_Exit);
+
+MODULE_LICENSE("GPL");
---
+++ 3/3_2/My_Kernel.c
@@ -0,0 +1,69 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <asm/current.h>
+#include <linux/uaccess.h>
+#include <linux/sched.h>
+
+#define procfs_name "Mythread_info"
+#define BUFSIZE  1024
+char buf[BUFSIZE]; //kernel buffer
+
+static ssize_t Mywrite(struct file *fileptr, const char __user *ubuf, size_t buffer_len, loff_t *offset){
+    /*Your code here*/
+    size_t to_copy = buffer_len;
+
+    if (to_copy >= BUFSIZE) to_copy = BUFSIZE - 1;
+
+    if (copy_from_user(buf, ubuf, to_copy))
+        return -EFAULT;
+
+    buf[to_copy] = '\0';
+    return buffer_len;
+    /****************/
+}
+
+
+static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len, loff_t *offset){
+    /*Your code here*/
+    int len = 0;
+    unsigned long ms;
+
+    if (*offset > 0) return 0;
+
+    // hint: current->utime/100/1000
+    ms = current->utime / 100 / 1000;
+
+    len = scnprintf(buf, BUFSIZE,
+                    "String: %s\nProcess ID: %d\nThread ID: %d\nTime(ms): %lu\n",
+                    buf, current->tgid, current->pid, ms);
+
+    if (len > buffer_len) len = buffer_len;
+    if (copy_to_user(ubuf, buf, len)) return -EFAULT;
+
+    *offset += len;
+    return len;
+    /****************/
+}
+
+static struct proc_ops Myops = {
+    .proc_read = Myread,
+    .proc_write = Mywrite,
+};
+
+static int My_Kernel_Init(void){
+    proc_create(procfs_name, 0644, NULL, &Myops);   
+    pr_info("My kernel says Hi");
+    return 0;
+}
+
+static void My_Kernel_Exit(void){
+    pr_info("My kernel says GOODBYE");
+}
+
+module_init(My_Kernel_Init);
+module_exit(My_Kernel_Exit);
+
+MODULE_LICENSE("GPL");
